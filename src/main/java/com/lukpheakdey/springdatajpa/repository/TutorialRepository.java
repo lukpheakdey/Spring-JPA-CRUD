2096e22ddc596d6fc25806391014eb225b887713
Luk Pheakdey <luk.pheakdey@gmail.com>
2021-05-18 13:33:11 -0400

    spring jpa data mysql CRUD

---
+++ src/main/java/com/lukpheakdey/springdatajpa/repository/TutorialRepository.java
@@ -0,0 +1,14 @@
+package com.lukpheakdey.springdatajpa.repository;
+
+import com.lukpheakdey.springdatajpa.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+
+}
